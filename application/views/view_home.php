919a0586034446f074c47a9f97f03778c606fbd1
Williams <w.contreras.f@gmail.com>
2015-06-25 09:05:25 -0300

    1er commit
    masivo de proyecto completo

---
+++ application/views/view_home.php
@@ -0,0 +1,37 @@
+<h1 class="page-header"><span class="glyphicon glyphicon-indent-left"></span> Bienvenido a Mini Pos</h1>
+<center><br/>
+<img src="<?php echo base_url()?>img/Logophp.png" />
+<BR/>
+Desarrollado por: Ing. Manuel Cortes Crisanto, Te Gusta mis Aportes Sigueme en: <a href="https://www.facebook.com/pages/Programacion-en-PHP/153571761391652">Programación en PHP</a>
+<br/><br/><br/>
+
+ 
+
+<div class="modal fade bs-example-modal-lg" id="test_modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+    	<center>
+    		<br/><br/><br/>
+     <h3>DONAR A PROGRAMACION EN PHP</h3>  <br/>  
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="K53VRY4DU2TVL">
+<input type="image" src="https://www.paypalobjects.com/es_XC/MX/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea.">
+<img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
+</form>
+<br/><br/>
+
+Gracias a tu Donacion conseguiremos un Nuevo Hosting seguiremos aportando software completamente gratuitos.</center>
+<br/><br/>
+    </div>
+  </div>
+</div>
+
+ 
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#test_modal').modal('show');
+    });
+</script>
+
